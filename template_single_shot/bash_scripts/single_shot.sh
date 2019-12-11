bebd2d8d686f2c6455da7b5844f44999f1c8cbb5
Steffen Dalgard <steffen.dalgard@sintef.no>
2019-12-11 17:15:27 +0100

    template dir for single shot container

---
+++ template_single_shot/bash_scripts/single_shot.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# arg1: run_file_name              
+# arg2: retry_max_count
+# arg3: code directory
+# arg4: input directory
+# arg5: work directory
+# arg6: output directory
+
+code_directory=${3}
+input_directory=${4}
+work_directory=${5}
+output_directory=${6}
+
+echo "code_directory: ${code_directory}"
+#echo "input_directory: ${input_directory}"
+echo "work_directory: ${work_directory}"
+echo "output_directory: ${output_directory}"
+echo '***'
+
+echo '#'
+echo '#  Starting Process: ...'
+echo '#'
+
+
+# Move the file(s) to output
+${code_directory}/move_to_output.sh ${output_directory} <output_file>
+
+echo '   Done'
+
