63535152562e6970e56b37f33a79c8e117b339c9
Steffen Dalgard <steffen.dalgard@sintef.no>
2019-12-11 17:27:07 +0100

    template dir for process many container

--- template_process_many/bash_scripts/process_job.sh
+++ template_process_many/bash_scripts/process_job.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-# arg1: run_file_name              
-# arg2: retry_max_count
-# arg3: code directory
-# arg4: input directory
-# arg5: work directory
-# arg6: output directory
+# arg1: work_path
+# arg2: code directory
+# arg3: input directory
+# arg4: work directory
+# arg5: output directory
 
-code_directory=${3}
-input_directory=${4}
-work_directory=${5}
-output_directory=${6}
+work_path=${1}
+code_directory=${2}
+input_directory=${3}
+work_directory=${4}
+output_directory=${5}
 
+echo "work_path: ${work_path}"
 echo "code_directory: ${code_directory}"
-#echo "input_directory: ${input_directory}"
+echo "input_directory: ${input_directory}"
 echo "work_directory: ${work_directory}"
 echo "output_directory: ${output_directory}"
 echo '***'
@@ -29,4 +30,5 @@ ${code_directory}/move_to_output.sh ${output_directory} <output_file>
 echo '#'
 echo '#  End Process'
 echo '#'
+
 
